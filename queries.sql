b1d5ae910639877381443905aa0a1dfe81a9366d
AounJafri <138041532+AounJafri@users.noreply.github.com>
2025-01-19 22:06:57 +1100

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,20 @@
+-- Create database ExpenseTracker;
+
+-- Create Table User(
+-- 	id integer auto_increment Primary key,
+-- 	username VARCHAR(55) UNIQUE Not null,
+--     password VARCHAR(100),
+--     email VARCHAR(30) Not null
+-- );
+
+
+
+-- Create Table Expense (
+-- 	id integer auto_increment Primary key,
+--     amount DECIMAL(8,2) NOT NULL,
+--     userid integer,
+--     description VARCHAR(255),
+--     category VARCHAR(30),
+--     date_col date,
+--     Foreign Key(userid) references User(id)
+-- )
